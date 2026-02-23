cfa079e5ea93fc7b165a6628d3b2547a1d2e9be2
sonumadhesiya <madheshiyas117@gmail.com>
2026-02-23 16:06:36 +0530

    updates changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import axios from 'axios';
+
+// Set global base URL for all axios requests
+axios.defaults.baseURL = 'https://devconnect-lkvo.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
